ee4e3e8d54d9241a1848e1579f8ab0fd43465da8
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2022-01-26 16:27:31 -0500

    Update analyzer.py
    Pdf worked, going back to txt

--- analyzer.py
+++ analyzer.py
@@ -55,7 +55,7 @@ def count_words(file_name):
 
 def main():
     # testing .txt file
-    word_list, word_frequency = count_words('test.pdf')
+    word_list, word_frequency = count_words('test.txt')
     print(word_list)
     print(word_frequency)
 
