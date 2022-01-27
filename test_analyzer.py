1d048e5ba7ffff551a396cf96ad607671d00092a
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2022-01-26 22:43:37 -0500

    Update test_analyzer.py
    Fixed E128 and W291 using linter

--- test_analyzer.py
+++ test_analyzer.py
@@ -1,7 +1,7 @@
 import analyzer
 
 expected_word_list_txt = ['i', 'have', 'been', 'one', 'acquainted',
-'with', 'the', 'night', 'walked', 'out', 'in', 'rain', 'and', 'back']
+                          'with', 'the', 'night', 'walked', 'out', 'in', 'rain', 'and', 'back']
 
 expected_word_frequency_txt = [2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1]
 
@@ -13,7 +13,7 @@ def test_analyzer_txt():
     word_list_txt, word_frequency_txt = analyzer.count_words('test.txt')
     assert expected_word_list_txt == word_list_txt and expected_word_frequency_txt == word_frequency_txt
 
-# Unit test for pdf 
+# Unit test for pdf
 def test_analyzer_pdf():
     word_list_pdf, word_frequency_pdf = analyzer.count_words('test.pdf')
     assert expected_word_list_pdf == word_list_pdf and expected_word_frequency_pdf == word_frequency_pdf
