e1906cb6b70df3017706b6bfcef6ececad7ea5a2
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2022-01-26 22:45:36 -0500

    Update test_analyzer.py
    Fixed E302 using linter

--- test_analyzer.py
+++ test_analyzer.py
@@ -8,11 +8,13 @@ expected_word_frequency_txt = [2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1]
 expected_word_list_pdf = ['some', 'say', 'the', 'world', 'will', 'end', 'in', 'fire', 'ice']
 expected_word_frequency_pdf = [2, 2, 1, 1, 1, 1, 2, 1, 1]
 
+
 # Unit test for txt
 def test_analyzer_txt():
     word_list_txt, word_frequency_txt = analyzer.count_words('test.txt')
     assert expected_word_list_txt == word_list_txt and expected_word_frequency_txt == word_frequency_txt
 
+    
 # Unit test for pdf
 def test_analyzer_pdf():
     word_list_pdf, word_frequency_pdf = analyzer.count_words('test.pdf')
